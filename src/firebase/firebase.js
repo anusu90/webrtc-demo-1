6692f9e7063d9a613f8576a5f79b790188ce9139
anusu90 <anusu90@gmail.com>
2021-03-31 16:13:44 +0530

    env variable

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,23 +1,14 @@
 import firebase from 'firebase';
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBgOj9MPE1vOzYp4Y4Ri-rDv5PekKQy2j4",
-    authDomain: "react-projects-618fc.firebaseapp.com",
-    projectId: "react-projects-618fc",
-    storageBucket: "react-projects-618fc.appspot.com",
-    messagingSenderId: "687667693794",
-    appId: "1:687667693794:web:aa81717e32b842cd7e76bb",
-    measurementId: "G-C2J38NR1VZ"
-};
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBgOj9MPE1vOzYp4Y4Ri-rDv5PekKQy2j4",
-    authDomain: "react-projects-618fc.firebaseapp.com",
-    projectId: "react-projects-618fc",
-    storageBucket: "react-projects-618fc.appspot.com",
-    messagingSenderId: "687667693794",
-    appId: "1:687667693794:web:e3964e78c207f0fd7e76bb",
-    measurementId: "G-H041HP75VC"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
 };
 
 firebase.initializeApp(firebaseConfig)
